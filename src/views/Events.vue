a5fdfe2bcc253c7e374dab3afe5d6d1ffb0b4f8c
Kourosh <kouroshmaleki@t-online.de>
2021-12-14 00:36:20 +0100

    events card

---
+++ src/views/Events.vue
@@ -0,0 +1,8 @@
+<div class="card bg-dark text-white">
+<img src="../assets/manchester.png" class="card-img" alt="Warehouse Project Manchester">
+<div class="card-img-overlay">
+  <h5 class="card-title">Warehouse Project Manchester</h5>
+  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+  <p class="card-text">Last updated 3 mins ago</p>
+</div>‚
+</div>
