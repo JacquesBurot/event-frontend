cecc8e48929ab588e2d53769a4c6ef3bc82b94e1
Kourosh <kouroshmaleki@t-online.de>
2021-12-26 20:51:19 +0100

    home page

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
     <router-link to="/events">Events</router-link>
+    <router-link to="/create">Create</router-link>
   </div>
   <router-view/>
   <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/845322208&color=%23040404&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/user-434371931" title="kouroshmaleki" target="_blank" style="color: #cccccc; text-decoration: none;">kouroshmaleki</a> · <a href="https://soundcloud.com/user-434371931/anti-racism-summer-set" title="summer set" target="_blank" style="color: #cccccc; text-decoration: none;">summer set</a></div>
@@ -20,19 +21,34 @@
 
 #nav {
   padding: 10px;
+
 }
 
 #nav a {
+
    /* Green */
+  text-decoration: none;
   border: none;
   color: darkred;
   padding: 20px 32px;
   text-align: center;
-  text-decoration: aliceblue;
+  text-underline-offset:7.2px;
 
+}
+
+#nav a:hover {
+  text-decoration: underline;
+  text-decoration-thickness: 2px;
 }
 
 #nav a.router-link-exact-active {
   color: darkred;
+
+}
+a:after {
+  content: "";
+  position:absolute;
+  background-color: white;
+
 }
 </style>
