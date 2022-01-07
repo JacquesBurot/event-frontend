840e0bca9c191ea001b2c6d3edf2f28da2df120b
JacquesBurot <77002521+JacquesBurot@users.noreply.github.com>
2022-01-07 14:29:47 +0100

    soundcloud Einbettung verändert

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@
     <router-link to="/create">Create</router-link>
   </div>
   <router-view/>
-  <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/845322208&color=%23040404&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/user-434371931" title="kouroshmaleki" target="_blank" style="color: #cccccc; text-decoration: none;">kouroshmaleki</a> · <a href="https://soundcloud.com/user-434371931/anti-racism-summer-set" title="summer set" target="_blank" style="color: #cccccc; text-decoration: none;">summer set</a></div>
 </template>
 
 <style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,12 +3,12 @@
   <section data-description="Super contrast">
     <div class="wrapper">
       <h1 class="delta uppercase thin raleway color-emphasis-2 ls-medium">
-        Create your own private<br>
+        Book your next<br>
         <span class="supersize montserrat bold">Event</span>
       </h1>
     </div>
   </section>
-
+  <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/845322208&color=%23040404&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/user-434371931" title="kouroshmaleki" target="_blank" style="color: #cccccc; text-decoration: none;">kouroshmaleki</a> · <a href="https://soundcloud.com/user-434371931/anti-racism-summer-set" title="summer set" target="_blank" style="color: #cccccc; text-decoration: none;">summer set</a></div>
 </template>
 
 <style>
